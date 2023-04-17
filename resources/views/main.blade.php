e33ea5b54fbb18e0b80fa8568f7551f2915ae020
Yurij M <mokruy9999@gmail.com>
2023-04-17 20:22:13 +0200

    add migrations and bootstrap

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,10 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
         <title>THE GAME</title>
-
     </head>
     <body class="antialiased">
+    <div class="container">
+        kjkjkj
+    </div>
     </body>
 </html>
